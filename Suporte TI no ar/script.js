4d1764b3c8ceb8752cfc6cc573a9a121de885ee4
HenriqueSilvaR <henriquehenrique8789@gmail.com>
2025-08-02 11:44:36 -0300

    Add files via upload

---
+++ Suporte TI no ar/script.js
@@ -0,0 +1,5 @@
+// Aguarda o carregamento completo da página
+document.addEventListener("DOMContentLoaded", () => {
+  // Fazendo a animação de fade-in no conteúdo
+  document.querySelector(".content").style.opacity = 1;
+});
